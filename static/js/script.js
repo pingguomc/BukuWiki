e2f0b29e2651e7218ddef66bf4ff8a68acd875c2
Creeper512 <creeper938@outlook.com>
2025-06-14 23:35:23 +0800

    根据浏览器主题自动切换暗色模式

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
         pangu.autoSpacingPage();
     }
 });
+
+const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+document.documentElement.setAttribute('data-theme-choice', isDarkMode ? 'dark' : 'light');
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  const isDark = e.matches;
+  document.documentElement.setAttribute('data-theme', isDark ? 'dark' : 'light');
+  document.documentElement.setAttribute('data-theme-choice', isDark ? 'dark' : 'light');
+});
